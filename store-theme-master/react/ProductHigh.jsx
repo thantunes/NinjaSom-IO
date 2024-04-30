7c9347bed93cd82e3b757cabb30ba5d39d820399
Guilherme <guilherme@evision.com.br>
2024-04-30 17:36:31 -0300

    fix

--- store-theme-master/react/ProductHigh.jsx
+++ store-theme-master/react/ProductHigh.jsx
@@ -1,38 +1,45 @@
-import { useState, useEffect } from 'react'
-import { useProduct } from 'vtex.product-context'
-import data2 from './components/mock/160.json'
-import data1 from './components/mock/159.json'
+import { useState, useEffect } from "react";
+import { useProduct } from "vtex.product-context";
 
 const ProductHigh = () => {
-    const [DataCollection1, setDataCollection1] = useState([])
-    const [DataCollection2, setDataCollection2] = useState([])
+  const [DataCollection1, setDataCollection1] = useState(false);
 
-    const { product } = useProduct()
-    useEffect(() => {
-        setDataCollection1(data1.Data)
-    }, [])
-    useEffect(() => {
-        setDataCollection2(data2.Data)
-    }, [])
-    // DataCollection1.map((prodc) => {
-    //     console.log({data:1, id:prodc.productId, ids:product.productId})
-    // })
-    // DataCollection2.map((prodc) => {
-    //     console.log({data:2, id:prodc.productId, ids:product.productId})
-    // })
-    return (<>
-        {
-            DataCollection1.map((prodc) => {
-                return `${prodc.ProductId}` == product.productId ? (<img width={76} src='https://ninjasom.vtexassets.com/assets/vtex.file-manager-graphql/images/e3c0d9bc-47fc-4cb5-b79a-0c9b0162a6e0___3cda9ff820b5c5346072073edbd38eb1.png' />) : null
-            })
-        }
-        {
-            DataCollection2.map((prodc) => {
-                return `${prodc.ProductId}` == product.productId ? (<img width={76} src='https://ninjasom.vtexassets.com/assets/vtex.file-manager-graphql/images/fd9c1d5b-465f-4efd-af40-5337a73f2cdf___502eb3b25cd243e8995a1d2a66b5eefa.png' />) : null
-            })
-        }
-    </>)
-}
+  const { product } = useProduct();
+  useEffect(() => {
+    setInterval(() => {
+      window.data59.length > 0 ? setDataCollection1(true): null
+    },50 );
+  }, []);
+  // DataCollection1.map((prodc) => {
+  //     console.log({data:1, id:prodc.productId, ids:product.productId})
+  // })
+  // DataCollection2.map((prodc) => {
+  //     console.log({data:2, id:prodc.productId, ids:product.productId})
+  // })
+  return (
+    <>
+      {DataCollection1 ? (
+        <>
+          {window?.data59?.map((prodc) => {
+            return `${prodc}` == product.productId ? (
+              <img
+                width={76}
+                src="https://ninjasom.vtexassets.com/assets/vtex.file-manager-graphql/images/e3c0d9bc-47fc-4cb5-b79a-0c9b0162a6e0___3cda9ff820b5c5346072073edbd38eb1.png"
+              />
+            ) : null;
+          })}
+          {window?.data60?.map((prodc) => {
+            return `${prodc}` == product.productId ? (
+              <img
+                width={76}
+                src="https://ninjasom.vtexassets.com/assets/vtex.file-manager-graphql/images/fd9c1d5b-465f-4efd-af40-5337a73f2cdf___502eb3b25cd243e8995a1d2a66b5eefa.png"
+              />
+            ) : null;
+          })}
+        </>
+      ) : null}
+    </>
+  );
+};
 
-
-export default ProductHigh
+export default ProductHigh;
--- store-theme-master/react/components/PriceDiscount-Summary/index.jsx
+++ store-theme-master/react/components/PriceDiscount-Summary/index.jsx
@@ -7,7 +7,7 @@ const PriceDisc = () => {
     const productPrice = product.sku.seller.commertialOffer.Price
     const linkShelf = product.link
 
-    console.log('Esse aq', product, skuSelector)
+    // console.log('Esse aq', product, skuSelector)
     const ValCalc = (productPrice - (productPrice * 0.08)).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
     const ValCalcRoland = (productPrice - (productPrice * 0.05)).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
     const SellerId = product?.items[0]?.sellers[1]?.sellerId == undefined ? "" : product.items[0]?.sellers[1]?.sellerId
@@ -18,7 +18,7 @@ const PriceDisc = () => {
 
         setTimeout(() => {
             if (document.readyState == 'complete') {
-                console.log('document', document)
+                // console.log('document', document)
                 const linkShelf = product.link
                 if (document?.querySelector(`[href="${linkShelf}"]`).getElementsByTagName('article')[0].getElementsByClassName('vtex-button')[0].getElementsByClassName('vtex-button__label')[0].getElementsByClassName('vtex-add-to-cart-button-0-x-buttonText')[0].innerHTML == 'Indisponível') {
                     setDomLoad(false)
@@ -29,14 +29,14 @@ const PriceDisc = () => {
     function CommertialRule() {
         if (productPrice > 100) {
             if (SellerId == undefined ? "" : SellerId == "Roland") {
-                console.log('rodou aq 2')
+                // console.log('rodou aq 2')
                 return <span style={{ display: DomLoad ? 'flex' : 'none', flexDirection: 'row', alignItems: 'baseline', gap: '5px', justifyContent: 'center' }} className='vtex-product-price-1-x-sellingPrice'><span style={{ fontSize: '24px', fontWeight: '700', lineHeight: '28px', color: '#010449', fontStyle: 'normal', fontFamily: '"Montserrat",sans-serif' }}>{ValCalcRoland.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</span></span>
             } else {
-                console.log('rodou aq')
+                // console.log('rodou aq')
                 return <span style={{ display: DomLoad ? 'flex' : 'none', flexDirection: 'row', alignItems: 'baseline', gap: '5px', justifyContent: 'center' }} className='vtex-product-price-1-x-sellingPrice'><span style={{ fontSize: '24px', fontWeight: '700', lineHeight: '28px', color: '#010449', fontStyle: 'normal', fontFamily: '"Montserrat",sans-serif' }}>{ValCalc}</span></span>
             }
         } else {
-            console.log('rodou aq 3')
+            // console.log('rodou aq 3')
             return <span style={{ display: DomLoad ? 'flex' : 'none', flexDirection: 'row', alignItems: 'baseline', gap: '5px', justifyContent: 'center' }} className='vtex-product-price-1-x-sellingPrice'><span style={{ fontSize: '24px', fontWeight: '700', lineHeight: '28px', color: '#010449', fontStyle: 'normal', fontFamily: '"Montserrat",sans-serif' }}>{productPrice.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</span></span>
         }
     }
